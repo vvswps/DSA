3d9612683806b002bdb330693d86e6d4520d2aa9
vvswps <a904465@gmail.com>
2022-07-22 22:06:03 +0530

    3 new questions

---
+++ longest_common_prefix.cpp
@@ -0,0 +1,18 @@
+// 14. Longest Common Prefix
+
+#include <iostream>
+
+int main()
+{
+    std::string strs[] = {"flower", "flow", "flight"};
+    // strs = ["c", "acc", "ccc"]
+    // strs = ["dog","racecar","car"]
+
+    // for (int i = 0; i < sizeof(strs); i++)
+    // {
+    //     std::cout << strs[i] << i << std::endl;
+    // }
+    std::cout << sizeof(strs) / sizeof(strs[0]) << std::endl;
+}
+
+// an array of strings
